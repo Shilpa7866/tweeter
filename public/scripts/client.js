7803fcf2f2d269eb22ec7ae4a770479ddbb23113
Shilpa <shilpa.pulipati24@gmail.com>
2023-06-03 23:46:35 -0400

    added code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,81 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+//Creating the tweets
+const createTweetElement = function (tweet) {
+  let $tweet =
+    `<article class = "border-tweet">
+    <header class = "header-tweet">
+      <img class = "header-userphoto" src= "${tweet.user.avatars}"></img>
+      <span class="header-name">${tweet.user.name}</span>
+      <span class="header-username">${tweet.user.handle}</span>
+    </header>
+    <p class="tweet-sentence">${escape(tweet.content.text)}</p>
+    <span class = "tweet-line"></span>
+    <footer class = "footer-tweet">
+      <a> ${timeago.format(tweet.created_at)} </a>
+      <div class = "tweet-button">
+        <button class = "btn"><i class="fa-solid fa-flag"></i></button>
+        <button class = "btn"><i class="fa-solid fa-retweet"></i></button>
+        <button class = "btn"><i class="fa-solid fa-heart"></i></button>
+      </div>
+    </footer>
+  </article>`
+  return $tweet;
+
+}
+
+//Will take an array of tweet objects and then appending each one to the #tweets-container.
+
+const renderTweets = function (tweets) {
+  for (const tweet in tweets) {
+    const tweetdata = createTweetElement(tweets[tweet]);
+    $('#container-tweet').prepend(tweetdata);
+  }
+}
+
+$(document).ready(function () {
+  $(".tweet-submit").submit(function (event) {
+    event.preventDefault();
+    if ($("#tweet-text").val().length > 140) {
+      $(".errormessage").html(`Error: Tweet must be less than 140 characters!`).slideDown().slideUp(10000);
+    } else if ($("#tweet-text").val().length === 0) {
+      $(".errormessage").html(`Error: Your Tweet cannot be Empty!!!`).slideDown().slideUp(10000);
+    } else {
+      $(".errormessage").html(``);
+      //Serialize the form data and send it to the server as a query string.
+      $.post('/tweets', $(this).serialize()).then(function () {
+        //calling ajax to get the last submission
+        $.ajax('/tweets', { method: 'GET' }).then(function (data) {
+          const tweetdata = createTweetElement(data[data.length - 1]);
+          $('#container-tweet').prepend(tweetdata);
+          document.getElementById("tweet-text").value = '';
+        });
+      });
+    }
+  });
+});
+
+const escape = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+
+
+
+// fetching tweets from the /tweets route, using Ajax
+function loadTweets() {
+  $.ajax('/tweets', { method: 'GET' })
+    .then(function (data) {
+      renderTweets(data);
+    });
+}
+
+//Fetching all the tweets at Page load
+loadTweets();
+
+
+
